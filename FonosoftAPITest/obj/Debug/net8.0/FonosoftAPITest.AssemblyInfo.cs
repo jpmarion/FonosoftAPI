6ee253700d8fb9e3e18012cdb5670cd489276d06
Juan Pablo Marion <totomarion@gmail.com>
2024-12-12 19:21:21 -0300

    Crear nuevo .gitignore

--- FonosoftAPITest/obj/Debug/net8.0/FonosoftAPITest.AssemblyInfo.cs
+++ FonosoftAPITest/obj/Debug/net8.0/FonosoftAPITest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FonosoftAPITest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e14d8e977c8ef17bed80db1030878c5ebd1ca61e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef78589cbd294e5052e7ea89ca46d2dcb2d9445a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FonosoftAPITest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FonosoftAPITest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
